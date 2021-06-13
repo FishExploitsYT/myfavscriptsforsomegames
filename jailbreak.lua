17b354edcbf780e9434ee9b39f81163b0fc312ed
FishExploitsYT <72241830+FishExploitsYT@users.noreply.github.com>
2021-06-13 14:28:38 +1000

    Create jailbreak.lua

---
+++ jailbreak.lua
@@ -0,0 +1,7 @@
+loadstring(game:GetObjects("rbxassetid://3473939051")[1].Source)()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))();
+wait(1)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+wait(1)
+print("scripts loaded")
